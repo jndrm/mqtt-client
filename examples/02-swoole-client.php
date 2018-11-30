5a257e55b22784de0d524ea1b5bebd96b55df21a
Donald <donald@drmer.net>
2018-11-30 09:50:16 +0800

    update examples

--- examples/02-swoole-client.php
+++ examples/02-swoole-client.php
@@ -6,16 +6,27 @@ require __DIR__ . '/../vendor/autoload.php';
 
 $client = SwooleClient::v4();
 
-$client->debug = true;
+// $client->debug = true;
 
 $client->connect('127.0.0.1', 1883, [
     'clientId' => 'swoole-client',
+    'willTopic' => 'hello/world',
+    'willQos' => 1,
+    'willMessage' => 'hello',
+    'keepAlive' => 10,
 ]);
 
 $client->on('connected', function () use ($client) {
-    // $client->subscribe('test', 2);
-    $client->publish('hello/world', 'Message from SwooleClient', 2);
-    // sleep(10);
+    $client->subscribe('test', 2);
+    $client->subscribe('hello/world', 1);
+    $client->publish('test', 'Message from ReactClient with Qos 0', 0);
+    $client->publish('test', 'Message from SwooleClient with Qos 1', 1);
+    $client->publish('test', 'Message from SwooleClient with Qos 2', 2);
     // $client->disconnect();
     // $client->close();
+});
+
+$client->on('message', function ($event, $packet) {
+    // Here be your own proccess message logic
+    echo $packet->getTopic() . ': ' . $packet->getPayload() . "\n";
 });
