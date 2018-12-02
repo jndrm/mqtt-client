33653e99d276d2dcb666c2a7d8784152cf0cde94
Donald <donald@drmer.net>
2018-12-03 07:28:02 +0800

    add connect test for SimpleClient

--- tests/SimpleClientTest.php
+++ tests/SimpleClientTest.php
@@ -23,4 +23,11 @@ class SimpleClientTest extends TestCase
         $instance = SimpleClient::v4();
         $this->assertInstanceOf(SimpleClient::class, $instance);
     }
+
+    public function testConnect()
+    {
+        $client = SimpleClient::v4();
+        $client->connect('127.0.0.1', 1883);
+        $this->assertTrue(true);
+    }
 }
